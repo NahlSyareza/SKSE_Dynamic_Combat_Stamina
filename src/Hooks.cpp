6135b6d8c48101c2d5ee8540a5175fa0d41688b8
NahlSyareza <availablearner@protonmail.com>
2025-02-03 18:00:44 +0700

    Removed unnecessary updates

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -3,35 +3,6 @@
 using namespace RE;
 using namespace std;
 
-bool IsRecoiling(RE::Actor* actor) {
-    bool state = false;
-    return actor->GetGraphVariableBool("IsRecoiling", state) && state;
-}
-
-bool IsRightCasting(RE::Actor* actor) {
-    bool state = false;
-    return actor->GetGraphVariableBool("IsCastingRight", state) && state;
-}
-
-bool IsLeftCasting(RE::Actor* actor) {
-    bool state = false;
-    return actor->GetGraphVariableBool("IsCastingLeft", state) && state;
-}
-
-bool IsDualCasting(RE::Actor* actor) {
-    bool state = false;
-    return actor->GetGraphVariableBool("IsCastingDual", state) && state;
-}
-
-bool IsCasting(RE::Actor* actor) { return IsRightCasting(actor) || IsLeftCasting(actor) || IsDualCasting(actor); }
-
-bool IsDrawing(RE::Actor* actor) { return actor->AsActorState()->GetAttackState() == RE::ATTACK_STATE_ENUM::kBowDrawn; }
-
-bool IsAttacking(RE::Actor* actor) {
-    bool state = false;
-    return actor->GetGraphVariableBool("IsAttacking", state) && state;
-}
-
 /**
  * Doesn't work with god mode
  * Currently not supporting stamina reducing perks
@@ -117,9 +88,6 @@ float Hooks::CombatStamina::ActionStaminaCost(RE::ActorValueOwner* avOwner, RE::
             logger::info("{} swings with {}", actor->GetName(), weapon->GetName());
             staminaCostDmg = (weapon->GetWeight() * swingMult) + swingBase;
 
-            /**
-             * Is this grouping the comparators legal???
-             */
             logger::info("Cost before: {}", staminaCostDmg);
             BGSEntryPoint::HandleEntryPoint(BGSEntryPoint::ENTRY_POINT::kModPowerAttackStamina, actor, weapon, addressof(staminaCostDmg));
             logger::info("Cost after: {}", staminaCostDmg);
@@ -142,98 +110,7 @@ P.S. (No, it's the fucking godmode)
 Either stamina draining or block breaking mechanic
 */
 
-void Hooks::CombatHit::HitImpact(RE::Actor* target, RE::HitData& hitData) {
-    Actor* aggressor = hitData.aggressor.get().get();
-
-    /*If there is no target or aggressor, return*/
-    if (!target || !aggressor || target->IsDead()) {
-        logger::info("Target or aggresor not found");
-        _HitImpact(target, hitData);
-        return;
-    }
-
-    /*
-    Is this the answer?
-    if (target->IsPlayerRef() && IsRecoiling(target)) {
-        float mult = hitData.flags.any(RE::HitData::Flag::kPowerAttack) ? 0.15F : 0.5F;
-        TryStagger(target, mult, aggressor);
-    }
-    */
-
-    /*Only registers if it is a bash*/
-    if (hitData.flags.any(RE::HitData::Flag::kBash)) {
-        /*Blocked bash do no damage*/
-        if (hitData.flags.any(RE::HitData::Flag::kBlocked)) {
-            hitData.totalDamage = 0;
-            _HitImpact(target, hitData);
-            return;
-        }
-
-        logger::info("{} bashed {}", aggressor->GetName(), target->GetName());
-
-        float staminaDamage = hitData.totalDamage;
-
-        logger::info("Stamina damage {}", staminaDamage);
-
-        target->AsActorValueOwner()->RestoreActorValue(RE::ACTOR_VALUE_MODIFIER::kDamage, RE::ActorValue::kStamina, -staminaDamage);
-
-        hitData.totalDamage = 0;
-    }
-
-    _HitImpact(target, hitData);
-}
+void Hooks::CombatHit::HitImpact(RE::Actor* target, RE::HitData& hitData) { _HitImpact(target, hitData); }
 
 // Change this with effect-applying restriction
-bool Hooks::CombatAction::DoCombatAction(RE::TESActionData* actData) {
-    TESObjectREFR* actorRef = actData->source.get();
-
-    if (!actData || !actorRef) {
-        return _DoCombatAction(actData);
-    }
-
-    Actor* actor = actorRef->As<RE::Actor>();
-
-    if (!actor) {
-        return _DoCombatAction(actData);
-    }
-
-    float staminaVal = actor->AsActorValueOwner()->GetActorValue(RE::ActorValue::kStamina);
-
-    const char* aEvent = actData->animEvent.c_str();
-
-    if (strstr(aEvent, "attack")) {
-        bool isLeft = strstr(aEvent, "Left");
-
-        const char* attackMsg = isLeft ? "left attack!" : "right attack!";
-
-        logger::info("Bro wants to {}!", attackMsg);
-
-        TESForm* attack = actor->GetEquippedObject(isLeft);
-
-        if (!attack) {
-            logger::info("No attack");
-            return _DoCombatAction(actData);
-        }
-
-        if (staminaVal < attack->GetWeight()) {
-            return false;
-        }
-    }
-
-    if (strstr(aEvent, "bash")) {
-        logger::info("Bro wants to bash!");
-
-        RE::TESForm* bash = actor->GetEquippedObject(getEquippedShield(actor));
-
-        if (!bash) {
-            logger::info("No bash?");
-            return _DoCombatAction(actData);
-        }
-
-        if (staminaVal < bash->GetWeight()) {
-            return false;
-        }
-    }
-
-    return _DoCombatAction(actData);
-}
+bool Hooks::CombatAction::DoCombatAction(RE::TESActionData* actData) { return _DoCombatAction(actData); }
