7c5f0c9c759010a69605b0d9511022f408c517f0
NahlSyareza <availablearner@protonmail.com>
2025-02-15 08:32:52 +0700

    Limits NPC to not attack when stamina is 0

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -1,14 +1,16 @@
 #include "Hooks.h"
 
+#include "mini/ini.h"
+
 using namespace RE;
-using namespace std;
+// using namespace std;
 
 /**
  * Doesn't work with god mode
  */
 
-float Hooks::CombatStamina::ActionStaminaCost(RE::ActorValueOwner* avOwner, RE::BGSAttackData* atkData) {
-    RE::Actor* actor = skyrim_cast<RE::Actor*>(avOwner);
+float Hooks::CombatStamina::ActionStaminaCost(ActorValueOwner* avOwner, BGSAttackData* atkData) {
+    Actor* actor = skyrim_cast<Actor*>(avOwner);
 
     if (!actor) {
         logger::info("Actor not found!");
@@ -17,13 +19,13 @@ float Hooks::CombatStamina::ActionStaminaCost(RE::ActorValueOwner* avOwner, RE::
 
     logger::info("{} detected", actor->GetName());
 
-    auto gameSettings = RE::GameSettingCollection::GetSingleton();
+    auto gameSettings = GameSettingCollection::GetSingleton();
 
     // How can I get custom GMST pls TwT
-    RE::Setting* normalStaminaMult = gameSettings->GetSetting("fDCSNormalStaminaMult");
-    RE::Setting* normalStaminaBase = gameSettings->GetSetting("fDCSNormalStaminaBase");
-    RE::Setting* powerStaminaMult = gameSettings->GetSetting("fDCSPowerStaminaMult");
-    RE::Setting* powerStaminaBase = gameSettings->GetSetting("fDCSPowerStaminaBase");
+    Setting* normalStaminaMult = gameSettings->GetSetting("fDCSNormalStaminaMult");
+    Setting* normalStaminaBase = gameSettings->GetSetting("fDCSNormalStaminaBase");
+    Setting* powerStaminaMult = gameSettings->GetSetting("fDCSPowerStaminaMult");
+    Setting* powerStaminaBase = gameSettings->GetSetting("fDCSPowerStaminaBase");
 
     if (normalStaminaMult) {
         logger::info("Normal mult: {}", normalStaminaMult->GetFloat());
@@ -31,7 +33,7 @@ float Hooks::CombatStamina::ActionStaminaCost(RE::ActorValueOwner* avOwner, RE::
         logger::info("Could not find GMST");
     }
 
-    float staminaCostDmg = 5.0F;
+    float costBase = 5.0F;
 
     float swingMult = 1.0F;
     float swingBase = 0.0F;
@@ -41,10 +43,10 @@ float Hooks::CombatStamina::ActionStaminaCost(RE::ActorValueOwner* avOwner, RE::
 
     float powerMult = 5.0F;
 
-    const char* isLeft = atkData->IsLeftAttack() ? "Left" : "Right";
-    logger::info("{} attack registered", isLeft);
+    // const char* isLeft = atkData->IsLeftAttack() ? "Left" : "Right";
+    // logger::info("{} attack registered", isLeft);
 
-    bool hasShield = getEquippedShield(actor);
+    bool hasShield = Hooks::getEquippedShield(actor);
 
     // if (hasShield) {
     //     logger::info("Is equipped with shield!");
@@ -52,7 +54,7 @@ float Hooks::CombatStamina::ActionStaminaCost(RE::ActorValueOwner* avOwner, RE::
     //     logger::info("Is not equipped with shield!");
     // }
 
-    if (atkData->data.flags.any(RE::AttackData::AttackFlag::kBashAttack)) {
+    if (atkData->data.flags.any(AttackData::AttackFlag::kBashAttack)) {
         logger::info("Start bashing");
 
         /*If bashing*/
@@ -64,7 +66,7 @@ float Hooks::CombatStamina::ActionStaminaCost(RE::ActorValueOwner* avOwner, RE::
 
         if (!equip) {
             logger::info("Can't detect bash object");
-            return _ActionStaminaCost(avOwner, atkData);
+            return costBase * atkData->data.staminaMult;
         }
 
         if (equip->IsArmor()) {
@@ -74,8 +76,8 @@ float Hooks::CombatStamina::ActionStaminaCost(RE::ActorValueOwner* avOwner, RE::
             float eff = av / 2;
             logger::info("Efficiency {}% (Block skill {})", floor(eff), av);
             logger::info("Bashes with {}", shield->GetName());
-            staminaCostDmg = (shield->GetWeight() * bashMult) + bashBase;
-            staminaCostDmg *= (1 - (floor(eff) / 100));
+            costBase = (shield->GetWeight() * bashMult) + bashBase;
+            costBase *= (1 - (floor(eff) / 100));
 
             // Potential error
             BGSEntryPoint::HandleEntryPoint(BGSEntryPoint::ENTRY_POINT::kModPowerAttackStamina, actor, shield, &perkMult);
@@ -99,8 +101,8 @@ float Hooks::CombatStamina::ActionStaminaCost(RE::ActorValueOwner* avOwner, RE::
                 logger::info("You shouldn't be here!");
             }
 
-            staminaCostDmg = (weapon->GetWeight() * bashMult) + bashBase;
-            staminaCostDmg *= (1 - (floor(eff) / 100));
+            costBase = (weapon->GetWeight() * bashMult) + bashBase;
+            costBase *= (1 - (floor(eff) / 100));
 
             // Potential error
             BGSEntryPoint::HandleEntryPoint(BGSEntryPoint::ENTRY_POINT::kModPowerAttackStamina, actor, weapon, &perkMult);
@@ -109,19 +111,19 @@ float Hooks::CombatStamina::ActionStaminaCost(RE::ActorValueOwner* avOwner, RE::
             logger::info("Bashes with {} ???", equip->GetName());
         }
 
-        if (atkData->data.flags.any(RE::AttackData::AttackFlag::kPowerAttack)) {
+        if (atkData->data.flags.any(AttackData::AttackFlag::kPowerAttack)) {
             logger::info("Is Power");
 
-            logger::info("Initial cost: {}", staminaCostDmg);
-            logger::info("Final cost: {}", staminaCostDmg * powerMult * perkMult * atkData->data.staminaMult);
+            logger::info("Initial cost: {}", costBase);
+            logger::info("Final cost: {}", costBase * powerMult * perkMult * atkData->data.staminaMult);
 
-            return staminaCostDmg * powerMult * perkMult * atkData->data.staminaMult;
+            return costBase * powerMult * perkMult * atkData->data.staminaMult;
         }
 
-        logger::info("Initial cost: {}", staminaCostDmg);
-        logger::info("Final cost: {}", staminaCostDmg * atkData->data.staminaMult);
+        logger::info("Initial cost: {}", costBase);
+        logger::info("Final cost: {}", costBase * atkData->data.staminaMult);
 
-        return staminaCostDmg * atkData->data.staminaMult;
+        return costBase * atkData->data.staminaMult;
     } else {
         logger::info("Start swinging");
         /**
@@ -134,10 +136,10 @@ float Hooks::CombatStamina::ActionStaminaCost(RE::ActorValueOwner* avOwner, RE::
 
         if (!equip) {
             logger::info("Can't detect swing object");
-            return _ActionStaminaCost(avOwner, atkData);
+            return costBase * atkData->data.staminaMult;
         }
 
-        TESObjectWEAP* weapon = equip->GetObject()->As<RE::TESObjectWEAP>();
+        TESObjectWEAP* weapon = equip->GetObject()->As<TESObjectWEAP>();
         logger::info("Swings with {}", weapon->GetName());
 
         float av = 0;
@@ -155,31 +157,48 @@ float Hooks::CombatStamina::ActionStaminaCost(RE::ActorValueOwner* avOwner, RE::
         }
 
         float actualWeight = weapon->GetWeight() < 1 ? 5 : weapon->GetWeight();
-        staminaCostDmg = (actualWeight * swingMult) + swingBase;
-        staminaCostDmg *= (1 - (floor(eff) / 100));
+        costBase = (actualWeight * swingMult) + swingBase;
+        costBase *= (1 - (floor(eff) / 100));
 
         BGSEntryPoint::HandleEntryPoint(BGSEntryPoint::ENTRY_POINT::kModPowerAttackStamina, actor, weapon, &perkMult);
         logger::info("Perk mult: {}", perkMult);
 
-        if (atkData->data.flags.any(RE::AttackData::AttackFlag::kPowerAttack)) {
+        if (atkData->data.flags.any(AttackData::AttackFlag::kPowerAttack)) {
             logger::info("Is Power", actor->GetName());
-            // staminaCostDmg *= powerMult;
+            // costBase *= powerMult;
 
-            logger::info("Initial cost: {}", staminaCostDmg);
-            logger::info("Final cost: {}", staminaCostDmg * powerMult * perkMult * atkData->data.staminaMult);
+            logger::info("Initial cost: {}", costBase);
+            logger::info("Final cost: {}", costBase * powerMult * perkMult * atkData->data.staminaMult);
 
-            return staminaCostDmg * powerMult * perkMult * atkData->data.staminaMult;
+            return costBase * powerMult * perkMult * atkData->data.staminaMult;
         }
 
-        logger::info("Initial cost: {}", staminaCostDmg);
-        logger::info("Final cost: {}", staminaCostDmg * atkData->data.staminaMult);
+        logger::info("Initial cost: {}", costBase);
+        logger::info("Final cost: {}", costBase * atkData->data.staminaMult);
 
-        return staminaCostDmg * atkData->data.staminaMult;
+        return costBase * atkData->data.staminaMult;
     }
 
     return _ActionStaminaCost(avOwner, atkData);
 }
 
 // Currently unused
-void Hooks::CombatHit::HitImpact(RE::Actor* target, RE::HitData& hitData) { _HitImpact(target, hitData); }
-bool Hooks::CombatAction::DoCombatAction(RE::TESActionData* actData) { return _DoCombatAction(actData); }
+void Hooks::CombatHit::HitImpact(Actor* target, HitData& hitData) { _HitImpact(target, hitData); }
+bool Hooks::CombatAction::DoCombatAction(TESActionData* actData) {
+    Actor* actor = actData->source.get()->As<Actor>();
+
+    if (!actor) {
+        return _DoCombatAction(actData);
+    }
+
+    float stamina = actor->AsActorValueOwner()->GetActorValue(ActorValue::kStamina);
+    const char* event = actData->animEvent.c_str();
+
+    if (strstr(event, "attack") && stamina <= 0) {
+        logger::info("{} tries to attack, but have no stamina!", actor->GetName());
+
+        return false;
+    }
+
+    return _DoCombatAction(actData);
+}
